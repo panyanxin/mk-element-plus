4ba8b1fa0abdf0a886839865f4036eab6832e3d9
panyanxin <1205307815@qq.com>
2022-06-30 15:12:28 +0800

    封装省市区组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,7 +14,11 @@ const routes: RouteRecordRaw[] = [
       {
         path: '/chooseIcon',
         component: () => import('../views/chooseIcon/index.vue')
-      }
+      },
+      {
+        path: '/chooseArea',
+        component: () => import('../views/chooseArea/index.vue')
+      },
     ] 
   }
 ]
---
+++ src/views/chooseArea/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <m-choose-area @change="changeArea"></m-choose-area>
+</template>
+
+<script lang='ts' setup>
+import mChooseArea from './../../components/chooseArea/src/index.vue'
+
+let changeArea = (val: any) => {
+  console.log(val)
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
