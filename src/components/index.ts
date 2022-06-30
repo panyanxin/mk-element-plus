624d38a3b644614777c326b978931bc64f67c903
panyanxin <1205307815@qq.com>
2022-06-30 17:25:33 +0800

    趋势组件封装

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,12 @@
 import { App } from 'vue'
 import chooseArea from './chooseArea'
 import chooseIcon from './chooseIcon'
+import trend from './trend'
 
 const components = [
   chooseArea,
   chooseIcon,
+  trend
 ]
 
 export default {
