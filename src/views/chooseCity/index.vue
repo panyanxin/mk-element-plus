d57541c4572bd5a54f3019bd63347945a9f88d92
panyanxin <1205307815@qq.com>
2022-07-08 13:14:40 +0800

    封装 城市选择

---
+++ src/views/chooseCity/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <m-choose-city @change="changeArea"></m-choose-city>
+</template>
+
+<script lang='ts' setup>
+
+let changeArea = (val: any) => {
+  console.log(val)
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
