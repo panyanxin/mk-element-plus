c90d5fff55ce6db5de303575a12e393bccb187ce
panyanxin <1205307815@qq.com>
2022-07-09 09:41:13 +0800

    表单组件 增加上传类型及操作项

--- src/components/form/src/index.vue
+++ src/components/form/src/index.vue
@@ -1,5 +1,6 @@
 <template>
   <el-form 
+    ref="form"
     v-if="model"
     :model="model" 
     :rules="rules" 
@@ -16,11 +17,31 @@
         :label="item.label"
       >
         <component 
+          v-if="item.type !== 'upload' && item.type !== 'editor'"
           v-bind="item.attrs"
           v-model="model[item.prop!]"
           :placeholder="item.placeholder" 
           :is="`el-${item.type}`"
         ></component>
+        <el-upload
+          v-else-if="item.type === 'upload'"
+          v-bind="item.uploadAttrs"
+          :on-preview="onPreview"
+          :on-remove="onRemove"
+          :on-success="onSuccess"
+          :on-error="onError"
+          :on-progress="onProgress"
+          :on-change="onChange"
+          :before-upload="beforeUpload"
+          :before-remove="beforeRemove"
+          :http-request="httpRequest"
+          :on-exceed="onExceed"
+        >
+          <slot name="uploadArea"></slot>
+          <template #tip>
+            <slot name="uploadTip"></slot>
+          </template>
+        </el-upload>
       </el-form-item>
       <el-form-item
         v-if="item.children && item.children.length"
@@ -43,23 +64,33 @@
         </component>
       </el-form-item>
     </template>
+    <el-form-item>
+      <slot name="action"  :form="form" :model="model"></slot>
+    </el-form-item>
   </el-form>
 </template>
 
 <script setup lang="ts">
 import { PropType,ref, onMounted, watch, nextTick } from 'vue';
-import { FormOptions } from './types/types';
+import { FormInstance, FormOptions } from './types/types';
 
 import cloneDeep from 'lodash/cloneDeep'
+
+let emits = defineEmits(['on-preview', 'on-remove', 'on-success', 'on-error', 'on-progress', 'on-change', 'before-upload', 'before-remove', 'on-exceed'])
 
 const props = defineProps({
   options: {
     type: Array as PropType<FormOptions[]>,
     require: true
+  },
+  // 用户自定义上传方法
+  httpRequest: {
+    type: Function
   }
 })
 let model = ref<any>(null)
 let rules = ref<any>(null)
+let form = ref<FormInstance | null>()
 
 // 初始化表单
 let initForm = () => {
@@ -78,6 +109,39 @@ let initForm = () => {
 onMounted(() => {
   initForm()
 })
+
+// 上传组件的所有方法
+let onPreview = (file: File) => {
+  emits('on-preview', file)
+}
+let onRemove = (file: File, fileList: FileList) => {
+  emits('on-remove', { file, fileList })
+}
+let onSuccess = (response: any, file: File, fileList: FileList) => {
+  // 上传图片成功 给表单上传项赋值
+  let uploadItem = props.options!.find(item => item.type === 'upload')!
+  model.value[uploadItem.prop!] = { response, file, fileList }
+  emits('on-success', { response, file, fileList })
+}
+let onError = (err: any, file: File, fileList: FileList) => {
+  emits('on-error', { err, file, fileList, })
+}
+let onProgress = (event: any, file: File, fileList: FileList) => {
+  emits('on-progress', { event, file, fileList })
+}
+let onChange = (file: File, fileList: FileList) => {
+  emits('on-change', { file, fileList })
+}
+let beforeUpload = (file: File) => {
+  emits('before-upload', file)
+}
+let beforeRemove = (file: File, fileList: FileList) => {
+  emits('before-remove', { file, fileList })
+}
+let onExceed = (files: File, fileList: FileList) => {
+  emits('on-exceed', { files, fileList })
+}
+
 </script>
 
 <style scoped>
--- src/components/form/src/types/types.ts
+++ src/components/form/src/types/types.ts
@@ -3,6 +3,12 @@
 import { CSSProperties } from 'vue'
 import { RuleItem } from "./rule"
 
+
+import { ValidateFieldsError } from 'async-validator'
+interface Callback {
+  (isValid?: boolean, invalidFields?: ValidateFieldsError): void,
+}
+
 // 表单每一项的配置选项
 export interface FormOptions {
    // 表单项显示的元素
@@ -30,4 +36,49 @@ export interface FormOptions {
   },
   // 表单项的子元素
   children?: FormOptions[],
+  // 处理上传组件的属性和方法
+  uploadAttrs?: {
+    action: string,
+    headers?: object,
+    method?: 'post' | 'put' | 'patch',
+    multiple?: boolean,
+    data?: any,
+    name?: string,
+    withCredentials?: boolean,
+    showFileList?: boolean,
+    drag?: boolean,
+    accept?: string,
+    thumbnailMode?: boolean,
+    fileList?: any[],
+    listType?: 'text' | 'picture' | 'picture-card',
+    autoUpload?: boolean,
+    disabled?: boolean,
+    limit?: number,
+  }
+}
+
+export interface ValidateFieldCallback {
+  (message?: string, invalidFields?: ValidateFieldsError): void,
+}
+
+export interface FormInstance {
+  registerLabelWidth(width: number, oldWidth: number): void,
+  deregisterLabelWidth(width: number): void,
+  autoLabelWidth: string | undefined,
+  emit: (evt: string, ...args: any[]) => void,
+  labelSuffix: string,
+  inline?: boolean,
+  model?: Record<string, unknown>,
+  size?: string,
+  showMessage?: boolean,
+  labelPosition?: string,
+  labelWidth?: string,
+  rules?: Record<string, unknown>,
+  statusIcon?: boolean,
+  hideRequiredAsterisk?: boolean,
+  disabled?: boolean,
+  validate: (callback?: Callback) => Promise<boolean>,
+  resetFields: () => void,
+  clearValidate: (props?: string | string[]) => void,
+  validateField: (props: string | string[], cb: ValidateFieldCallback) => void,
 }
